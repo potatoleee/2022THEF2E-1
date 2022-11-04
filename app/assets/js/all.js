bef6459a6d0c9d336eef045882e20f8d61e3e1a9
potatolee <dean19990226@gmail.com>
2022-11-04 14:18:07 +0800

    gsap impot success

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,11 +1,37 @@
+// import { gsap } from "../../../gsap/dist/gsap";
+// import { ScrollTrigger } from "../../../gsap/dist/ScrollTrigger";
+// import { TextPlugin } from "../../../gsap/dist/TextPlugin";
+// import { gsap, ScrollTrigger, TextPlugin } from "../../node_modules/gsap/all.js";
+
 // const { gsap } = require("../../../node_modules/gsap/dist/gsap");
 // const { ScrollTrigger } = require("../../../node_modules/gsap/dist/ScrollTrigger");
 // const { TextPlugin } = require("../../../node_modules/gsap/dist/TextPlugin");
 
-// gsap.registerPlugin(ScrollTrigger, TextPlugin);
+// import { gsap, ScrollTrigger, TextPlugin } from "../../../node_modules/gsap/all.js";
+// 上面那些都不需要
 
-const { gsap } = require("../node_modules/gsap/dist/gsap");
-const { ScrollTrigger } = require("../node_modules/gsap/dist/ScrollTrigger");
-const { TextPlugin } = require("../node_modules/gsap/dist/TextPlugin");
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
-gsap.to('.cursor', {opacity:0, duration:1, repeat: -1})
+
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".box1",
+      markers: true,
+      start: 'top 35%',
+      end: 'top 1%',
+      scrub: true,
+    },
+  })
+
+tl.to('.box1', {
+    top: 0,
+    left: '50%',
+    xPercent: '-50',
+    // duration: 10,
+    position: 'absolute',
+  }).to('.box1', {
+    top: '100%',
+    yPercent: '-100',
+    // duration: 20,
+    position: 'absolute',
+  })
