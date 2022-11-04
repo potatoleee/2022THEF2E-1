4649a7e43e04abd3cfc179907d0559b8be8a1c1e
potatolee <dean19990226@gmail.com>
2022-11-04 15:55:38 +0800

    Update 2022-11-04T07:55:36.189Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,8 +8,10 @@
 // const { ScrollTrigger } = require("../../../node_modules/gsap/dist/ScrollTrigger");
 // const { TextPlugin } = require("../../../node_modules/gsap/dist/TextPlugin");
 // import { gsap, ScrollTrigger, TextPlugin } from "../../../node_modules/gsap/all.js";
-// 上面那些都不需要
-gsap.registerPlugin(ScrollTrigger, TextPlugin);
+//上面那些官方建議的import方式都不需要了，我想是因為加上這段的原因 `${nodePath}/gsap/dist/all.js`
+//但如果有增加 gsap 其他的套件一樣要新增這段註冊，也不需要加上 type="module"; 但我目前還不知道真正原因，自己測試出來的，助教沒解釋道這部分。
+gsap.registerPlugin(ScrollTrigger, TextPlugin); //套件選填
+
 var tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".box1",
