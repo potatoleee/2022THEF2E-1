33ec70dcfe6ec6bb4713b9dcfaafe5befdd01f6d
potatolee <dean19990226@gmail.com>
2022-11-04 14:18:28 +0800

    Update 2022-11-04T06:18:25.727Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,22 +1,34 @@
 "use strict";
 
+// import { gsap } from "../../../gsap/dist/gsap";
+// import { ScrollTrigger } from "../../../gsap/dist/ScrollTrigger";
+// import { TextPlugin } from "../../../gsap/dist/TextPlugin";
+// import { gsap, ScrollTrigger, TextPlugin } from "../../node_modules/gsap/all.js";
 // const { gsap } = require("../../../node_modules/gsap/dist/gsap");
 // const { ScrollTrigger } = require("../../../node_modules/gsap/dist/ScrollTrigger");
 // const { TextPlugin } = require("../../../node_modules/gsap/dist/TextPlugin");
-// gsap.registerPlugin(ScrollTrigger, TextPlugin);
-var _require = require("../node_modules/gsap/dist/gsap"),
-    gsap = _require.gsap;
-
-var _require2 = require("../node_modules/gsap/dist/ScrollTrigger"),
-    ScrollTrigger = _require2.ScrollTrigger;
-
-var _require3 = require("../node_modules/gsap/dist/TextPlugin"),
-    TextPlugin = _require3.TextPlugin;
-
+// import { gsap, ScrollTrigger, TextPlugin } from "../../../node_modules/gsap/all.js";
+// 上面那些都不需要
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
-gsap.to('.cursor', {
-  opacity: 0,
-  duration: 1,
-  repeat: -1
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".box1",
+    markers: true,
+    start: 'top 35%',
+    end: 'top 1%',
+    scrub: true
+  }
+});
+tl.to('.box1', {
+  top: 0,
+  left: '50%',
+  xPercent: '-50',
+  // duration: 10,
+  position: 'absolute'
+}).to('.box1', {
+  top: '100%',
+  yPercent: '-100',
+  // duration: 20,
+  position: 'absolute'
 });
 //# sourceMappingURL=all.js.map
